cbe78c35b960c506db3873c3da65cd5d450f6d74
shkwak-hedy <shkwak@hedy.kr>
2024-05-08 16:27:08 +0900

    설정중

--- camera_example/main/take_picture.c
+++ camera_example/main/take_picture.c
@@ -34,6 +34,7 @@
 #include <nvs_flash.h>
 #include <sys/param.h>
 #include <string.h>
+#include "who_camera.h"
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -96,37 +97,74 @@
 static const char *TAG = "example:take_picture";
 
 #if ESP_CAMERA_SUPPORTED
+// static camera_config_t camera_config = {
+//     .pin_pwdn = CAM_PIN_PWDN,
+//     .pin_reset = CAM_PIN_RESET,
+//     .pin_xclk = CAM_PIN_XCLK,
+//     .pin_sccb_sda = CAM_PIN_SIOD,
+//     .pin_sccb_scl = CAM_PIN_SIOC,
+
+//     .pin_d7 = CAM_PIN_D7,
+//     .pin_d6 = CAM_PIN_D6,
+//     .pin_d5 = CAM_PIN_D5,
+//     .pin_d4 = CAM_PIN_D4,
+//     .pin_d3 = CAM_PIN_D3,
+//     .pin_d2 = CAM_PIN_D2,
+//     .pin_d1 = CAM_PIN_D1,
+//     .pin_d0 = CAM_PIN_D0,
+//     .pin_vsync = CAM_PIN_VSYNC,
+//     .pin_href = CAM_PIN_HREF,
+//     .pin_pclk = CAM_PIN_PCLK,
+
+//     //XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental)
+//     .xclk_freq_hz = 20000000,
+//     .ledc_timer = LEDC_TIMER_0,
+//     .ledc_channel = LEDC_CHANNEL_0,
+
+//     .pixel_format = PIXFORMAT_RGB565, //YUV422,GRAYSCALE,RGB565,JPEG
+//     .frame_size = FRAMESIZE_QVGA,    //QQVGA-UXGA, For ESP32, do not use sizes above QVGA when not JPEG. The performance of the ESP32-S series has improved a lot, but JPEG mode always gives better frame rates.
+
+//     .jpeg_quality = 12, //0-63, for OV series camera sensors, lower number means higher quality
+//     .fb_count = 1,       //When jpeg mode is used, if fb_count more than one, the driver will work in continuous mode.
+//     .fb_location = CAMERA_FB_IN_PSRAM,
+//     .grab_mode = CAMERA_GRAB_WHEN_EMPTY,
+// };
+
 static camera_config_t camera_config = {
-    .pin_pwdn = CAM_PIN_PWDN,
-    .pin_reset = CAM_PIN_RESET,
-    .pin_xclk = CAM_PIN_XCLK,
-    .pin_sccb_sda = CAM_PIN_SIOD,
-    .pin_sccb_scl = CAM_PIN_SIOC,
+  .pin_pwdn = CAMERA_PIN_PWDN,
+  .pin_reset = CAMERA_PIN_RESET,
+  .pin_xclk = CAMERA_PIN_XCLK,
+  .pin_sccb_sda = CAMERA_PIN_SIOD,
+  .pin_sccb_scl = CAMERA_PIN_SIOC,
 
-    .pin_d7 = CAM_PIN_D7,
-    .pin_d6 = CAM_PIN_D6,
-    .pin_d5 = CAM_PIN_D5,
-    .pin_d4 = CAM_PIN_D4,
-    .pin_d3 = CAM_PIN_D3,
-    .pin_d2 = CAM_PIN_D2,
-    .pin_d1 = CAM_PIN_D1,
-    .pin_d0 = CAM_PIN_D0,
-    .pin_vsync = CAM_PIN_VSYNC,
-    .pin_href = CAM_PIN_HREF,
-    .pin_pclk = CAM_PIN_PCLK,
+  .pin_d7 = CAMERA_PIN_D7,
+  .pin_d6 = CAMERA_PIN_D6,
+  .pin_d5 = CAMERA_PIN_D5,
+  .pin_d4 = CAMERA_PIN_D4,
+  .pin_d3 = CAMERA_PIN_D3,
+  .pin_d2 = CAMERA_PIN_D2,
+  .pin_d1 = CAMERA_PIN_D1,
+  .pin_d0 = CAMERA_PIN_D0,
+  .pin_vsync = CAMERA_PIN_VSYNC,
+  .pin_href = CAMERA_PIN_HREF,
+  .pin_pclk = CAMERA_PIN_PCLK,
 
-    //XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental)
-    .xclk_freq_hz = 20000000,
-    .ledc_timer = LEDC_TIMER_0,
-    .ledc_channel = LEDC_CHANNEL_0,
+  //XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental)
+  .xclk_freq_hz = XCLK_FREQ_HZ,
+  .ledc_timer = LEDC_TIMER_0,
+  .ledc_channel = LEDC_CHANNEL_0,
+  /**
+   * format을 JPEG로 설정하면, RGP565일 때 보다 속도가 빠르며 
+   * 연속 요청시 종료되지 않음. 상황에 맞게 사용할 것
+  */
+  .pixel_format = PIXFORMAT_RGB565, //YUV422,GRAYSCALE,RGB565,JPEG
+  //.pixel_format = PIXFORMAT_JPEG,
+  .frame_size = FRAMESIZE_QVGA, //QQVGA-UXGA, For ESP32, do not use sizes above QVGA when not JPEG. The performance of the ESP32-S series has improved a lot, but JPEG mode always gives better frame rates.
 
-    .pixel_format = PIXFORMAT_RGB565, //YUV422,GRAYSCALE,RGB565,JPEG
-    .frame_size = FRAMESIZE_QVGA,    //QQVGA-UXGA, For ESP32, do not use sizes above QVGA when not JPEG. The performance of the ESP32-S series has improved a lot, but JPEG mode always gives better frame rates.
-
-    .jpeg_quality = 12, //0-63, for OV series camera sensors, lower number means higher quality
-    .fb_count = 1,       //When jpeg mode is used, if fb_count more than one, the driver will work in continuous mode.
-    .fb_location = CAMERA_FB_IN_PSRAM,
-    .grab_mode = CAMERA_GRAB_WHEN_EMPTY,
+  .jpeg_quality = 12,           //0-63, for OV series camera sensors, lower number means higher quality
+  .fb_count = 1,                //When jpeg mode is used, if fb_count more than one, the driver will work in continuous mode.
+  .fb_location = CAMERA_FB_IN_PSRAM,
+  .grab_mode = CAMERA_GRAB_WHEN_EMPTY,
 };
 
 static esp_err_t init_camera(void)
